e4ccfcc4a48bd262a067457506ab54aceea59500
toby7002 <144540995+toby7002@users.noreply.github.com>
2024-03-04 17:54:13 +0700

    Done

--- common/src/main/java/com/toby7002/ecww/CommonClass.java
+++
@@ -1,5 +0,0 @@
-package com.toby7002.ecww;
-public class CommonClass {
-    public static void init() {
-    }
-}
--- forge/src/main/java/com/toby7002/ecww/EnoughCustomWorldWarningForge.java
+++ forge/src/main/java/com/toby7002/ecww/EnoughCustomWorldWarningForge.java
@@ -5,15 +5,5 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(Constants.MOD_ID)
 public class EnoughCustomWorldWarningForge {
     
-    public EnoughCustomWorldWarningForge() {
-    
-        // This method is invoked by the Forge mod loader when it is ready
-        // to load your mod. You can access Forge and Common code in this
-        // project.
-    
-        // Use Forge to bootstrap the Common mod.
-        Constants.LOG.info("Hello Forge world!");
-        CommonClass.init();
-        
-    }
+    public EnoughCustomWorldWarningForge() {}
 }
